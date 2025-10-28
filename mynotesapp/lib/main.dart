79228e033845aed4c7661f45f44ecdbe8ef05f9e
kishor N <kishorns9606@gmail.com>
2025-10-28 14:53:40 +0530

    step-2

--- mynotesapp/lib/main.dart
+++ mynotesapp/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:mynotesapp/firebase_options.dart';
 import 'package:mynotesapp/views/login_view.dart';
 import 'package:mynotesapp/views/register_view.dart';
+import 'package:mynotesapp/views/verify_email_view.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
@@ -34,13 +35,17 @@ class HomePage extends StatelessWidget {
         builder: (context, snapshot){
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-            // final user = (FirebaseAuth.instance.currentUser);
-            // if (user?.emailVerified ?? false) {
-            //  return const Text('Done');               
-            // } else {  
-            //  return const VerifyEmailView();
-            //}
-            return const LoginView();
+            final user = FirebaseAuth.instance.currentUser;
+            if (user !=null){
+              if (user.emailVerified){
+                print('Email is verified');
+              }else{
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return const Text('Done');
             default:
             return const CircularProgressIndicator();
             }
