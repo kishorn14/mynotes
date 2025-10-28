6939ab8336993225d535625368431e0f3604948d
kishor N <kishorns9606@gmail.com>
2025-10-28 14:36:07 +0530

    step-2

--- mynotesapp/lib/main.dart
+++ mynotesapp/lib/main.dart
@@ -3,18 +3,21 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotesapp/firebase_options.dart';
 import 'package:mynotesapp/views/login_view.dart';
-
+import 'package:mynotesapp/views/register_view.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
     MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-      
+      theme: ThemeData(      
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: false,
       ),
       home: const HomePage(),
+      routes: {
+        '/login/': (context) => const LoginView(),
+        '/register/': (context) => const RegisterView(),
+      },
     ),
     );
 }
@@ -24,31 +27,24 @@ class HomePage extends StatelessWidget {
 
  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: FutureBuilder(
+    return  FutureBuilder(
         future: Firebase.initializeApp(
           options: DefaultFirebaseOptions.currentPlatform,
           ),
         builder: (context, snapshot){
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-            final user = (FirebaseAuth.instance.currentUser);
-            if (user?.emailVerified ?? false) {
-              print('You are a verified user');
-            } else {
-              print('You need to verify your email first');
+            // final user = (FirebaseAuth.instance.currentUser);
+            // if (user?.emailVerified ?? false) {
+            //  return const Text('Done');               
+            // } else {  
+            //  return const VerifyEmailView();
+            //}
+            return const LoginView();
+            default:
+            return const CircularProgressIndicator();
             }
-            return const Text('Done');
-        default:
-        return const Text('Loading...');
-
-          }
-         
         },
-      ),
-    );
+      );
   }
 }
